c9cac06778d57ade7e5803a27fe6984ea8b5caa1
subeen <soobin2318@gmail.com>
2024-10-26 16:51:53 +0900

    deploy 1026

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // req.body가 객체로 인식됨
 app.use("/api", indexRouter);
 
-const mongoURI = process.env.LOCAL_DB_ADDRESS;
-//const mongoURI = process.env.DEPLOY_DB_ADDRESS;
+//const mongoURI = process.env.LOCAL_DB_ADDRESS;
+const mongoURI = process.env.DEPLOY_DB_ADDRESS;
 mongoose
   .connect(mongoURI)
   .then(() => console.log("mongoose connected"))
